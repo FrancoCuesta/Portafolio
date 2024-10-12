2deb94bd07d0413084d123afcb13dcfdc32eea5b
FrancoCuesta <cuestafranco2@gmail.com>
2024-10-12 01:27:50 -0300

    Deploy the app with GitHub Pages

---
+++ src/app/components/nanv/nanv.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nanv',
+  standalone: true,
+  imports: [],
+  templateUrl: './nanv.component.html',
+  styleUrl: './nanv.component.scss'
+})
+export class NanvComponent {
+
+}
